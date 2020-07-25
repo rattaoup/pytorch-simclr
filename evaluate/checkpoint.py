32ffdbebe7d61c274e809fece231660e9bbc304e
rpukdee <36057197+rattaoup@users.noreply.github.com>
2020-07-25 15:01:00 +0700

    Update checkpoint.py
    add num epochs for to checkpoint

--- evaluate/checkpoint.py
+++ evaluate/checkpoint.py
@@ -19,8 +19,7 @@ def save_checkpoint(net, clf, critic, epoch, args, script_name):
     destination = os.path.join('./checkpoint', args.filename)
     torch.save(state, destination)
 
-
-def save_checkpoint2(net, clf, critic, epoch, args, script_name, base_optimizer, encoder_optimizer):
+def save_checkpoint2(net, clf, critic, epoch, args, script_name, base_optimizer, encoder_optimizer, num_epochs):
     # Save checkpoint.
     print('Saving..')
     state = {
@@ -31,7 +30,8 @@ def save_checkpoint2(net, clf, critic, epoch, args, script_name, base_optimizer,
         'args': vars(args),
         'script': script_name,
         'base_optim': base_optimizer.state_dict(),
-        'encoder_optim': encoder_optimizer.state_dict()
+        'encoder_optim': encoder_optimizer.state_dict(),
+        'num_epochs': num_epochs
     }
     if not os.path.isdir('checkpoint'):
         os.mkdir('checkpoint')
