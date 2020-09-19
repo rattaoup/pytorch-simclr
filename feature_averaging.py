dbdfe19f9bff5b9f46ace608d07426131ab1b26b
ae-foster <adamfosterrules@hotmail.co.uk>
2020-09-19 15:20:06 +0100

    make fa work here

--- feature_averaging.py
+++ feature_averaging.py
@@ -6,6 +6,7 @@ import torch.backends.cudnn as cudnn
 import math
 import os
 import argparse
+from tqdm import tqdm
 
 from augmentation import ColourDistortion, TensorNormalise, ModuleCompose
 from models import *
@@ -19,10 +20,12 @@ parser.add_argument("--layer", type=int, default=-1, help='Layer of the network 
 parser.add_argument("--type", type=str, choices=['linear', 'mlp'], default='linear', help='Classifier type')
 parser.add_argument("--num-hidden", type=int, default=2048, help='For MLP, how many hidden units?')
 parser.add_argument("--regularization", type=int, choices=[1, 2], default=2, help='Choose between L1 and L2 regularization')
-parser.add_argument("--num-passes", type=int, default=10, help='Number of passes to average')
-parser.add_argument("--reg-lower", type=float, default=-7, help='Minimum log regularization parameter (base 10)')
-parser.add_argument("--reg-upper", type=float, default=-3, help='Maximum log regularization parameter (base 10)')
-parser.add_argument("--num-steps", type=int, default=16, help='Number of log-linearly spaced reg parameters to try')
+parser.add_argument("--max-num-passes", type=int, default=20, help='Max number of passes to average')
+parser.add_argument("--min-num-passes", type=int, default=10, help='Min number of passes to average')
+parser.add_argument("--step-num-passes", type=int, default=2, help='Number of distinct M values to try')
+parser.add_argument("--reg-lower", type=float, default=-6, help='Minimum log regularization parameter (base 10)')
+parser.add_argument("--reg-upper", type=float, default=-4, help='Maximum log regularization parameter (base 10)')
+parser.add_argument("--num-steps", type=int, default=4, help='Number of log-linearly spaced reg parameters to try')
 args = parser.parse_args()
 
 # Load checkpoint.
@@ -76,17 +79,44 @@ batch_transform = ModuleCompose([
     ]).to(device)
 
 
-def encode_feature_averaging(loader, device, net, num_passes=10):
+def encode_feature_averaging(clftrainloader, device, net, target=None, num_passes=10):
+    if target is None:
+        target = device
+
+    net.eval()
+
+    X, y = [], None
+    with torch.no_grad():
+        for i in tqdm(range(num_passes)):
+            store = []
+            for batch_idx, (inputs, targets) in enumerate(clftrainloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                rn = col_distort.sample_random_numbers(inputs.shape, inputs.device)
+                inputs = batch_transform(inputs, rn)
+                representation = net(inputs)
+                representation, targets = representation.to(target), targets.to(target)
+                store.append((representation, targets))
+
+            Xi, y = zip(*store)
+            Xi, y = torch.cat(Xi, dim=0), torch.cat(y, dim=0)
+            X.append(Xi)
+
+    X = torch.stack(X, dim=0)
+
+    return X, y
     
 
-
-best_acc = 0
 X, y = encode_feature_averaging(clftrainloader, device, net, num_passes=args.num_passes)
 X_test, y_test = encode_feature_averaging(testloader, device, net, num_passes=args.num_passes)
-for reg_weight in torch.exp(math.log(10) * torch.linspace(args.reg_lower, args.reg_upper, args.num_steps,
-                                                          dtype=torch.float, device=device)):
-    clf = train_clf(X, y, net.representation_dim, num_classes, device, reg_weight=reg_weight)
-    acc, loss = test_matrix(X_test, y_test, clf)
-    if acc > best_acc:
-        best_acc = acc
-print("Best test accuracy", best_acc, "%")
+for m in torch.linspace(args.min_num_passes, args.max_num_passes, args.step_num_passes, dtype=torch.int):
+    print("FA with M =", m)
+    best_acc = 0
+    for reg_weight in torch.exp(math.log(10) * torch.linspace(args.reg_lower, args.reg_upper, args.num_steps,
+                                                              dtype=torch.float, device=device)):
+        X = X[:m, ...].mean(0) / m
+        X_test = X_test[:m, ...].mean(0) / m
+        clf = train_clf(X, y, net.representation_dim, num_classes, device, reg_weight=reg_weight)
+        acc, loss = test_matrix(X_test, y_test, clf)
+        if acc > best_acc:
+            best_acc = acc
+    print("Best test accuracy", best_acc, "%")
