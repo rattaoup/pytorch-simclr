34b094d5d8034a382ac30152ef7afed12541d3db
ae-foster <adamfosterrules@hotmail.co.uk>
2020-09-19 14:31:23 +0100

    Remove cut-off, num-epochs now fulfills this role

--- invariance_by_gradient.py
+++ invariance_by_gradient.py
@@ -38,7 +38,6 @@ parser.add_argument("--test-freq", type=int, default=10, help='Frequency to fit 
                                                               'classifier only training here.')
 parser.add_argument("--save-freq", type=int, default=100, help='Frequency to save checkpoints.')
 parser.add_argument("--filename", type=str, default='ckpt.pth', help='Output file name')
-parser.add_argument("--cut-off", type=int, default=1000, help='last epoch')
 parser.add_argument("--gp-upper-limit", type=float, default=1., help='Clip the gradient penalty from above at this '
                                                                      'value')
 parser.add_argument("--git", action='store_true', help="Record the git hash and diff (uses a subprocess call)")
@@ -181,7 +180,7 @@ def update_results(train_contrastive_loss, train_gradient_penalty, train_total_l
     results['test_acc'].append(test_acc)
 
 
-for epoch in range(start_epoch, min(args.cut_off,start_epoch + args.num_epochs)):
+for epoch in range(start_epoch, args.num_epochs):
     outputs = train(epoch)
     if (args.test_freq > 0) and (epoch % args.test_freq == (args.test_freq - 1)):
         X, y = encode_train_set(clftrainloader, device, net)
