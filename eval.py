d4b6f7d21e33be797bf9cefbcb36a7261a7f0b52
ae-foster <adamfosterrules@hotmail.co.uk>
2021-02-26 15:41:01 +0000

    eval script supports only one feature averaging, not sweep; eval script supports untransformed inputs for cifar

--- eval.py
+++ eval.py
@@ -48,11 +48,16 @@ def main(args):
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     task = 'reg' if args.dataset == 'spirograph' else 'clf'
+    if args.untransformed and args.num_passes > 1:
+        raise ValueError("Cannot perform feature averaging with untransformed inputs")
+    elif args.untransformed and args.dataset == 'spirograph':
+        raise ValueError("Spirograph does not support untransformed inputs")
+    aug = not args.untransformed
 
     # Data
     print('==> Preparing data..')
     _, testset, clftrainset, num_classes, stem, col_distort, batch_transform = get_datasets(
-        args.dataset, augment_clf_train=True, augment_test=True, train_proportion=args.proportion)
+        args.dataset, augment_clf_train=aug, augment_test=aug, train_proportion=args.proportion)
 
     testloader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False, num_workers=args.num_workers,
                                              pin_memory=True)
@@ -85,27 +90,25 @@ def main(args):
 
     batch_transform = batch_transform.to(device)
 
-    results = {}
     X, y = encode_feature_averaging(clftrainloader, device, net, col_distort, batch_transform,
-                                    num_passes=args.max_num_passes, target='cpu')
+                                    num_passes=args.num_passes, target='cpu')
     X_test, y_test = encode_feature_averaging(testloader, device, net, col_distort, batch_transform,
-                                              num_passes=args.max_num_passes, target='cpu')
-    for m in torch.linspace(args.min_num_passes, args.max_num_passes, args.step_num_passes):
-        m = int(m)
-        print("FA with M =", m)
-        X_this = X[:m, ...].mean(0)
-        X_test_this = X_test[:m, ...].mean(0)
-        if task == "clf":
-            clf = train_clf(X_this.to(device), y.to(device), net.representation_dim, num_classes, device,
-                            reg_weight=args.reg_weight)
-            acc, loss = test_matrix(X_test_this.to(device), y_test.to(device), clf)
-        elif task == "reg":
-            regressor = train_reg(X_this, y, device, reg_weight=args.reg_weight)
-            loss = test_reg(X_test_this, y_test, regressor)
-            acc = None
-        else:
-            raise ValueError("Unexpected task type: %s" % task)
-        results[m] = (acc, loss)
+                                              num_passes=args.num_passes, target='cpu')
+    if args.num_passes > 1:
+        print("Feature averaging with M =", args.num_passes)
+    X = X.mean(0)
+    X_test = X_test.mean(0)
+    if task == "clf":
+        clf = train_clf(X.to(device), y.to(device), net.representation_dim, num_classes, device,
+                        reg_weight=args.reg_weight)
+        acc, loss = test_matrix(X_test.to(device), y_test.to(device), clf)
+    elif task == "reg":
+        regressor = train_reg(X, y, device, reg_weight=args.reg_weight)
+        loss = test_reg(X_test, y_test, regressor)
+        acc = None
+    else:
+        raise ValueError("Unexpected task type: %s" % task)
+    results = (acc, loss)
     return results
 
 
@@ -114,10 +117,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tune regularization coefficient of downstream classifier.')
     parser.add_argument("--num-workers", type=int, default=2, help='Number of threads for data loaders')
     parser.add_argument("--load-from", type=str, default='ckpt.pth', help='File to load from')
-    parser.add_argument("--max-num-passes", type=int, default=20, help='Max number of passes to average')
-    parser.add_argument("--min-num-passes", type=int, default=10, help='Min number of passes to average')
-    parser.add_argument("--step-num-passes", type=int, default=2, help='Number of distinct M values to try')
+    parser.add_argument("--num-passes", type=int, default=1, help='Number of features to average')
     parser.add_argument("--reg-weight", type=float, default=1e-5, help='Regularization parameter')
     parser.add_argument("--proportion", type=float, default=1., help='Proportion of train data to use')
+    parser.add_argument("--untransformed", action="store_true", default=False,
+                        help="Use untransformed inputs")
     args = parser.parse_args()
     print(main(args))
